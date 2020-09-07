8b368ed8a2590ebd49bf6e3c1d4b08bba668df48
cazamudio90 <36246888+cazamudio90@users.noreply.github.com>
2020-09-07 20:38:37 -0300

    commit antes de windows form

--- Trabajo Practico/Entidades/Numero.cs
+++ Trabajo Practico/Entidades/Numero.cs
@@ -32,7 +32,7 @@ namespace Entidades
             this.numero = validarNumero(p_numero);
         }
 
-        private bool EsBinario(string p_binario)
+        private static bool EsBinario(string p_binario)
         {
             bool v_return = true;
             for (int i = 0; i < p_binario.Length; i++)
@@ -42,6 +42,50 @@ namespace Entidades
                     v_return = false;
                     break;
                 }
+            }
+            return v_return;
+        }
+
+        public static string binarioDecimal(string p_binary)
+        {
+
+            int num;
+            string v_return = "Valor invalido";
+            int binVal;
+            int decVal = 0;
+            int baseVal = 1;
+            int rem;
+
+            if (EsBinario(p_binary))
+            {
+                num = int.Parse(p_binary);
+                binVal = num;
+                while (num > 0)
+                {
+                    rem = num % 10;
+                    decVal = decVal + rem * baseVal;
+                    num = num / 10;
+                    baseVal = baseVal * 2;
+                }
+                v_return = decVal.ToString();
+            }
+            return v_return;
+        }
+
+        public static string decimalBinario(int p_num)
+        {
+            int[] a = new int[10];
+            int i;
+            string v_return = "Valor invalido";
+
+            for (i = 0; p_num > 0; i++)
+            {
+                a[i] = p_num % 2;
+                p_num = p_num / 2;
+            }
+            for (i = i - 1; i >= 0; i--)
+            {
+                v_return = v_return + a[i];
             }
             return v_return;
         }
