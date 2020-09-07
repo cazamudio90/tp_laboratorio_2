02b35518036cd8e155eb3dc1077dee24953f498c
cazamudio90 <36246888+cazamudio90@users.noreply.github.com>
2020-09-07 20:27:31 -0300

    antes de windows form
    ultimo antes de windows form

---
+++ Trabajo Practico/Entidades/Calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    static class Calculadora
+    {
+        public static string ValidarOperador(char p_operador)
+        {
+            string v_return;
+
+            switch (p_operador)
+            {
+                case '+':
+                    v_return = "+";
+                    break;
+                case '-':
+                    v_return = "-";
+                    break;
+                case '*':
+                    v_return = "*";
+                    break;
+                case '/':
+                    v_return = "/";
+                    break;
+                default:
+                    v_return = "+";
+                    break;
+            }
+            return v_return;
+        }
+    }
+}
---
+++ Trabajo Practico/Entidades/Numero.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        double numero;
+
+        Numero()
+        {
+            numero = 0;
+        }
+
+        private double validarNumero(string p_numero)
+        {
+            double aux_numero;
+            double v_return = 0;
+
+            if (double.TryParse(p_numero, out aux_numero))
+            {
+                v_return = aux_numero;
+            }
+            return v_return;
+        }
+
+        public void setNumero(string p_numero)
+        {
+            this.numero = validarNumero(p_numero);
+        }
+
+        private bool EsBinario(string p_binario)
+        {
+            bool v_return = true;
+            for (int i = 0; i < p_binario.Length; i++)
+            {
+                if (p_binario.Substring(i) != "0" && p_binario.Substring(i) != "1")
+                {
+                    v_return = false;
+                    break;
+                }
+            }
+            return v_return;
+        }
+    }
+}
