5bc7775aa1bb94f0bd9db087bf77c79e3e7c2499
cazamudio90 <36246888+cazamudio90@users.noreply.github.com>
2020-09-09 00:39:32 -0300

    Empezamos Diseño

---
+++ Trabajo Practico/MiCalculadora/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MiCalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.cbOperaciones = new System.Windows.Forms.ComboBox();
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnConvBinario = new System.Windows.Forms.Button();
+            this.btnConvDecimal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(125, 78);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(135, 20);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(538, 78);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(135, 20);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // cbOperaciones
+            // 
+            this.cbOperaciones.FormattingEnabled = true;
+            this.cbOperaciones.ItemHeight = 13;
+            this.cbOperaciones.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.cbOperaciones.Location = new System.Drawing.Point(327, 78);
+            this.cbOperaciones.Name = "cbOperaciones";
+            this.cbOperaciones.Size = new System.Drawing.Size(121, 21);
+            this.cbOperaciones.TabIndex = 2;
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(88, 199);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(146, 52);
+            this.btnOperar.TabIndex = 3;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(327, 199);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(146, 52);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(554, 199);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(146, 52);
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // btnConvBinario
+            // 
+            this.btnConvBinario.Location = new System.Drawing.Point(176, 299);
+            this.btnConvBinario.Name = "btnConvBinario";
+            this.btnConvBinario.Size = new System.Drawing.Size(146, 52);
+            this.btnConvBinario.TabIndex = 6;
+            this.btnConvBinario.Text = "Convertir a Binario";
+            this.btnConvBinario.UseVisualStyleBackColor = true;
+            // 
+            // btnConvDecimal
+            // 
+            this.btnConvDecimal.Location = new System.Drawing.Point(451, 299);
+            this.btnConvDecimal.Name = "btnConvDecimal";
+            this.btnConvDecimal.Size = new System.Drawing.Size(146, 52);
+            this.btnConvDecimal.TabIndex = 7;
+            this.btnConvDecimal.Text = "Convertir a Decimal";
+            this.btnConvDecimal.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvDecimal);
+            this.Controls.Add(this.btnConvBinario);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.Controls.Add(this.cbOperaciones);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Calculadora de Carlos Zamudio 2°D";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.ComboBox cbOperaciones;
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnConvBinario;
+        private System.Windows.Forms.Button btnConvDecimal;
+    }
+}
+
