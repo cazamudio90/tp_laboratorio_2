cafd7ae283c13c797d1641e221c1d69db2173d94
cazamudio90 <36246888+cazamudio90@users.noreply.github.com>
2020-10-27 22:31:41 -0300

    Hasta Clase Alumno

---
+++ Trabajo Practico 3/Zamudio.Carlos.2D/Entidades/NacionalidadInvalidaException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class NacionalidadInvalidaException : Exception
+    {
+        public NacionalidadInvalidaException(string mensaje) : base(mensaje)
+        {
+             Console.WriteLine(Message);
+        }
+    }
+}
